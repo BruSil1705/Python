0a5ddef5a8e06fae630169082ef1e6ae8ef0503d
Bruno Silva <117646884+BruSil1705@users.noreply.github.com>
2022-11-22 08:23:24 -0300

    Add files via upload

---
+++ Operações básicas.py
@@ -0,0 +1,8 @@
+#Elaborar um algoritmo para os valores a e b abaixo, 
+# que escreva sua soma, subtração, 
+# divisão e multiplicação. Utilizando variáveis.
+# A= 10 B= 5
+print(10+5)
+print(10-5)
+print(10*5)
+print(10/5)
