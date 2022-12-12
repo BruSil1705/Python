d816d838a5e78a3aa652ab98ac35f6bfdc233490
Bruno Silva <117646884+BruSil1705@users.noreply.github.com>
2022-12-12 11:56:06 -0300

    Add files via upload

---
+++ Python1.py
@@ -0,0 +1,78 @@
+#|IF, ELSE, INT(INPUT)
+'''idade = int(input("Digite sua idade"))
+genero= int(input("Qual seu gênero? 1- Masculino 2- Feminino"))
+
+if genero == 1:
+    h = 72
+    temrest= (h - idade)
+    print("Seu tempo restante é de:", temrest, "anos.")
+else:
+    m == 79
+    temrest= (m - idade)
+    print("Seu tempo restante é de:", temrest, "anos.")'''
+
+#|MATCH CASE
+'''print("Opções: 1- Maseratti 2- McLAren 3- Bugatti")
+op = int(input("Escolha um carro:"))
+match op:
+    case 1:
+        print("Potência: 460 cv")
+        print("Velocidade máxima: 298 km/h")
+    case 2:
+        print("Potência: 720 cv")
+        print("Velocidade máxima: 341 km/h")
+    case 3:
+        print("Potência: 1600 cv")
+        print("Velocidade máxima: 490 km/h")'''
+        
+#|WHILE
+
+
+
+
+
+#|FOR
+'''for x in range(inicio, fim, passo)'''
+'''for x in range(0, 100000, 10):
+    print(x)'''
+    
+#|REVERSE FOR
+
+'''for x in range(1000, 0, -1):
+    print(x)'''
+    
+#|LISTAS (VETORES)
+
+'''Declarar uma lista (ou Vetor)
+VisualG- Vetor: vetor[0..10] de inteiro
+Python- Nome qualquer = []
+Exemplo:
+nomes=[]
+Atribuir valores
+'Sempre começa do 0 ZERO
+nome = ["Bruno", "Andreia", 17, 5, 20, True]
+print(nome[1])
+print(nome)
+Comando LEN 
+mostra a quantidade de elementos da lista
+print("A lista possui", len(nome), "elementos.")'''
+
+
+
+#|FOR + LISTA
+'''for i in range(0, len(nome)):
+    print(nome[i])'''
+
+
+#|APPEND    
+'''contador = []
+for j in range(1000):
+    contador.append(j)
+print(contador[396])'''
+
+
+
+
+
+
+
